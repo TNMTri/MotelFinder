9d1c73e896ed707b78083af0aa7e68fd0b7cb9d6
TNMTri <tri.tran@softersoucre.com>
2015-03-23 11:01:17 +0700

    Update service.

--- Service/controllers/controllers.js
+++ Service/controllers/controllers.js
@@ -6,6 +6,9 @@ var mongoose = require('mongoose');
 
 var radius = 1;
 var number = 1;
+
+var init = true;
+
 var my_location = {
     lat: 10.767013,
     long: 106.690455
@@ -28,97 +31,113 @@ var controllers = {
 
     index: function (req, res) {
 
-        //Get all:
-        controllers.get_all_motel_array(req, res);
+        //Đưa ví trí hiện tại vào đây:
+//        my_location.lat = "";
+//        my_location.long = "";
 
-        setTimeout(function () {
+        //Lấy tất cả nhà trọ xung quanh, nếu database nhiều sẽ thay đổi thành lấy từng đoạn:
+        if (init == true) {
+            init = false;
+            controllers.get_all_motel_array(req, res);
 
-            req.session.motel_array_all.forEach(function (motel) {
-
-                var circumference = 40075.6; // Earth's circumference at the equator in km
-                var distance = 0.0;
-
-                //Calculate radians
-                var latitude1Rad = my_location.lat * Math.PI / 180.0;
-                var longitude1Rad = my_location.long * Math.PI / 180.0;
-                var latitude2Rad = motel.address.latitude * Math.PI / 180.0;
-                var longitude2Rad = motel.address.longitude * Math.PI / 180.0;
-
-                var longitudeDiff = Math.abs(longitude1Rad - longitude2Rad);
-
-                if (longitudeDiff > Math.PI) {
-                    longitudeDiff = 2.0 * Math.PI - longitudeDiff;
-                }
-
-                var angleCalculation = Math.acos(Math.sin(latitude2Rad) * Math.sin(latitude1Rad) + Math.cos(latitude2Rad) * Math.cos(latitude1Rad) * Math.cos(longitudeDiff));
-
-                distance = circumference * angleCalculation / (2.0 * Math.PI);
-                motel.distance = (distance * 1000).toFixed(2);
-            });
-
-            //sort
             setTimeout(function () {
-                for (i = 0; i < req.session.motel_array_all.length; i++) {
+
+                req.session.motel_array_all.forEach(function (motel) {
+
+                    var circumference = 40075.6; //Chu vi trái đất tính từ xích đạo.
+                    var distance = 0.0;
+
+                    //Calculate radians
+                    var latitude1Rad = my_location.lat * Math.PI / 180.0;
+                    var longitude1Rad = my_location.long * Math.PI / 180.0;
+                    var latitude2Rad = motel.address.latitude * Math.PI / 180.0;
+                    var longitude2Rad = motel.address.longitude * Math.PI / 180.0;
+
+                    var longitudeDiff = Math.abs(longitude1Rad - longitude2Rad);
+
+                    if (longitudeDiff > Math.PI) {
+                        longitudeDiff = 2.0 * Math.PI - longitudeDiff;
+                    }
+
+                    var angleCalculation = Math.acos(Math.sin(latitude2Rad) * Math.sin(latitude1Rad) + Math.cos(latitude2Rad) * Math.cos(latitude1Rad) * Math.cos(longitudeDiff));
+
+                    distance = circumference * angleCalculation / (2.0 * Math.PI);
+                    motel.distance = (distance * 1000).toFixed(5);
+                });
+
+                //Sắp xếp:
+                setTimeout(function () {
+                    for (i = 0; i < req.session.motel_array_all.length; i++) {
 //                    console.log(req.session.motel_array_all[i].distance);
-                    for (j = i + 1; j < req.session.motel_array_all.length; j++) {
+                        for (j = i + 1; j < req.session.motel_array_all.length; j++) {
 //                        console.log(req.session.motel_array_all[i].distance + " vs " + req.session.motel_array_all[j].distance);
-                        if ((req.session.motel_array_all[i].distance - req.session.motel_array_all[j].distance) > 0) {
-                            var temporary = req.session.motel_array_all[i];
-                            req.session.motel_array_all[i] = req.session.motel_array_all[j];
-                            req.session.motel_array_all[j] = temporary;
+                            if ((req.session.motel_array_all[i].distance - req.session.motel_array_all[j].distance) > 0) {
+                                var temporary = req.session.motel_array_all[i];
+                                req.session.motel_array_all[i] = req.session.motel_array_all[j];
+                                req.session.motel_array_all[j] = temporary;
+                            }
+                        }
+                    }
+                });
+
+
+                console.log("Start app.");
+                for (var i = motel_recent.length; i < req.session.motel_array_all.length; i++) {
+                    if (motel_recent.length == number * 10) {
+                        number++;
+                        res.render('index', {motel_array: motel_recent});
+                    } else {
+                        if (req.session.motel_array_all[i].distance <= radius * 500) {
+                            motel_recent.push(req.session.motel_array_all[i]);
+                        } else {
+                            radius++;
+                            motel_recent.push(req.session.motel_array_all[i]);
                         }
                     }
                 }
             });
-            setTimeout(function () {
-//                res.render('index', { title: req.session.motel_array_all});
-                controllers.get_more(req, res);
-            }, 1000)
-
-        });
+        } else {
+            res.render('index', {motel_array: motel_recent});
+        }
     },
 
     get_more: function (req, res) {
-
+        console.log("Đâm!");
         if (req.session.motel_array_all.length == motel_recent.length) {
-
-            res.render('index', {title: motel_recent, notification: "Không còn nhà trọ nào cả."});
-
+            console.log("Hết rồi.");
+            res.send(motel_recent);
         } else {
-
-            for (i = motel_recent.length; i < req.session.motel_array_all.length; i++) {
-
-//                console.log(motel_recent.length);
-
+            for (var i = motel_recent.length - 1; i < req.session.motel_array_all.length; i++) {
                 if (motel_recent.length == number * 10) {
-//                    console.log(number);
                     number++;
-                    res.render('index', {title: motel_recent});
-
+                    res.send(motel_recent);
                 } else {
-
                     if (req.session.motel_array_all[i].distance <= radius * 500) {
-
                         motel_recent.push(req.session.motel_array_all[i]);
-
                     } else {
-
                         radius++;
                         motel_recent.push(req.session.motel_array_all[i]);
-
                     }
                 }
             }
-
         }
-
     }
+
+//    ajax_get_more: function (req, res) {
+//        controllers.get_more(req, res, true);
+//    },
+
+//    my_location: function (req, res) {
+//
+//    }
 };
 
 module.exports = function (router) {
 
-    router.get('/', controllers.index);
-    router.get('/get_more', controllers.get_more);
+    router.get('/', controllers.index); //Vừa vào thì chạy cái này.
+    router.get('/get_more/', controllers.get_more);
+//    router.get('/ajax_get_more/', controllers.ajax_get_more);
+//    router.get('/my_location/', controllers.my_location);
 
     return router;
 };
